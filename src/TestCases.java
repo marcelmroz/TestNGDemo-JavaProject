9baa2f15f7f5127d0e197e91169bf4998f805023
Marcel Mroz <mmroz.marcel@gmail.com>
2023-10-05 17:52:56 +0200

    setup, testSteps, tearDown

--- src/TestCases.java
+++ src/TestCases.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class TestCases {
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    void setup(){
+        System.out.println("inside setup");
+    }
+
+    @Test
+    void testSteps(){
+        System.out.println("inside testSteps");
+    }
+
+    @Test
+    void tearDown(){
+        System.out.println("inside tearDown");
+    }
+
 }
